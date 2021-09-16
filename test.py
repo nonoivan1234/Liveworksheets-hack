2b8adb41c770cbace8fff5b06de3717064b75788
YuCheng Chang <71141033+nonoivan1234@users.noreply.github.com>
2021-09-16 22:26:05 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-text = "prompt' + document.getElementById('wswo').getAttribute('class').charAt(2) + 'd"
-
-text += "'"
-text = "'" + text
-text = text.replace("document.getElementById('wswo').getAttribute('class').charAt(", "'qwertyuiopasdfghjklzxcvbnm'[")
-text = text.replace(")", "]")
-
-print(text)
